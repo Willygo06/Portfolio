bec491b92d15569c941a1cd7ccc115ff3b37af74
Willygo06 <wilfried.gomes-fortes@epitech.eu>
2025-10-25 02:02:23 +0200

    ✨ add techstack

---
+++ frontend/portfolio/section/data/data.js
@@ -0,0 +1,47 @@
+export const TechStackData = [
+    {name: "NodeJS", logo: "../src/assets/nodejs-2.svg"},
+    {name: "Python", logo: "../src/assets/python-5.svg"},
+    {name: "Java", logo: "../src/assets/java-4.svg"},
+    {name: "Javascript", logo: "../src/assets/javascript-1.svg"},
+    {name: "ReactJS", logo: "../src/assets/react-2.svg"},
+    {name: "TypeScript", logo: "../src/assets/typescript-2.svg"},
+    {name: "React Native", logo: "../src/assets/react-2.svg"},
+    {name: "Tailwinds CSS", logo: "../src/assets/tailwind-css-2.svg"},
+    {name: "MySQL", logo: "../src/assets/mysql-3.svg"},
+    {name: "MongoDB", logo: "../src/assets/mongodb-icon-2.svg"},
+    {name: "Docker", logo: "../src/assets/docker.svg"}
+
+];
+
+export const projects = [
+    {
+        name: "ÉcoTrack",
+        notes: "2025 - Projet de fin d'année (Pré-Msc)- Epitech Nice",
+        description: "Application mobile de suivi des habitudes écologiques ludique.",
+        technologies: ["React Native", "NodeJS + ExpressJS", "MongoDB"],
+    },
+
+    {
+        name: "Projet IRC",
+        notes: "2025 - Projet scolaire - Epitech Nice",
+        description: "Plateforme web de chat en temps réel",
+        technologies: ["ReactJS", "NodeJS + ExpressJS", "Socket.IO", "MongoDB"],
+    },
+
+    {
+        name: "Quest",
+        notes: "2023 - Projet de fin de formation - La Capsule Nice",
+        description: "Plateforme web multi-service livraison en ligne.",
+        technologies: ["ReactJS + NextJS+ TailwindCSS", "NodeJS + ExpressJS", "MongoDB"],
+        link: "https://quest-frontend-chi.vercel.app/"
+    }
+
+]
+
+
+export const hobbies = [
+    "Football",
+    "Jeux Vidéo",
+    "Voyages",
+    "Technologie",
+];
--- frontend/portfolio/src/App.jsx
+++ frontend/portfolio/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
-import React from 'react'
+import react from 'react'
 import { Navbar } from '../section/partial/Navbar'
 import { Footer } from '../section/partial/Footer'
+import { TechStack } from '../section/partial/techStack.jsx'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <TechStack />
       <Footer />
     </>
   )
