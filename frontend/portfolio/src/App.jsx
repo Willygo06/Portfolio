0f9e9b0d8bbcd332599d9905e89b419a8cd58f4a
Willygo06 <wilfried.gomes-fortes@epitech.eu>
2025-11-03 00:51:35 +0100

    ✨  add intro animation + little update

--- frontend/portfolio/src/App.jsx
+++ frontend/portfolio/src/App.jsx
@@ -1,22 +1,30 @@
 import './App.css'
-import react from 'react'
-import { Navbar } from '../section/partial/Navbar'
-import { Footer } from '../section/partial/Footer'
+import React, { useState } from 'react'
+import { Navbar } from '../section/partial/Navbar.jsx'
+import { Footer } from '../section/partial/Footer.jsx'
 import { TechStack } from '../section/partial/techStack.jsx'
 import { Projects } from '../section/partial/project.jsx'
 import { Contact } from '../section/partial/contact.jsx'
 import { Hero } from '../section/partial/hero.jsx'
+import { IntroAnimation } from '../section/partial/introAnimation.jsx'
 
 function App() {
+  const [showIntro, setShowIntro] = useState(true);
 
   return (
     <>
-      <Navbar />
-      <Hero />
-      <TechStack />
-      <Projects />
-      <Contact />
-      <Footer />
+      {showIntro ? (
+        <IntroAnimation onFinish={() => setShowIntro(false)} />
+      ) : (
+        <div>
+          <Navbar />
+          <Hero />
+          <TechStack />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>
+      )}
     </>
   )
 }
